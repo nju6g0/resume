f9129c9e1c1cae8f236615ddfe3a321b198507b4
YiShan Su <nju6g0@gmail.com>
2022-10-15 00:49:30 +0800

    add change scene animation

--- src/Pages/Animation/SunAndMoon/SunAndMoon.js
+++ src/Pages/Animation/SunAndMoon/SunAndMoon.js
@@ -1,21 +1,34 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 import classes from "./styles.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(classes);
 
+const SUN = "sun";
+const MOON = "moon";
+
 const SunAndMoon = () => {
-  const containerRef = useRef(null);
+  const [nowScene, setNowScene] = useState(MOON);
+
+  const changeScene = (scene) => {
+    setNowScene(scene);
+  };
 
   return (
-    <div ref={containerRef} className={cx("wrapper")}>
-      <div className={cx("moon")} />
-      <div className={cx("sun")} />
-      <div className={cx("starsContainer")}>
+    <div className={cx("wrapper", nowScene)}>
+      <div className={cx("starsContainer", nowScene === MOON && "show")}>
         {Array.from({ length: 30 }, (x) => x).map((star, idx) => (
           <div key={`star${idx}`} className={`star${idx + 1}`} />
         ))}
       </div>
+      <div
+        className={cx("moon", nowScene === MOON && "show")}
+        onMouseUp={() => changeScene(SUN)}
+      />
+      <div
+        className={cx("sun", nowScene === SUN && "show")}
+        onMouseUp={() => changeScene(MOON)}
+      />
     </div>
   );
 };
