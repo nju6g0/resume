9bae57a90151840c723d1e6ce64d1669c146c50d
YiShan Su <nju6g0@gmail.com>
2022-11-09 23:08:25 +0800

    (f2e_web_topic) add scroll animation

--- src/Pages/TheF2E/Web2/Topic.js
+++ src/Pages/TheF2E/Web2/Topic.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import img_div from "image/F2E_web/topic_div.png";
 import img_design from "image/F2E_web/topic_design.png";
@@ -8,11 +8,28 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(classes);
 
 const Topic = () => {
+  const blockRef = useRef(null);
+  const [isReach, setIsReach] = useState(false);
+
+  useEffect(() => {
+    const offsetTop = blockRef.current?.offsetTop;
+    const handleScroll = () => {
+      if(window.scrollY >= offsetTop){
+        setIsReach(true);
+        document.removeEventListener("scroll", handleScroll);
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className={cx("topic")}>
+    <div ref={blockRef} className={cx("topic")}>
       <div className={cx("top")}>
         <p>互動式網頁設計</p>
-        <div>
+        <div {...(isReach && { className: cx("active") })}>
           <img src={img_div} alt="" />
           <img src={img_design} alt="" />
         </div>
