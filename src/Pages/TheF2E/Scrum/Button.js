9330bb8d5fb9fe2e94e73cab33436e79da085f49
YiShan Su <nju6g0@gmail.com>
2022-11-20 15:10:05 +0800

    (f2e_scrum) add component Button

---
+++ src/Pages/TheF2E/Scrum/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTyps from "prop-types";
+
+import classes from "./styles.module.scss";
+import classNames from "classnames/bind";
+const cx = classNames.bind(classes);
+
+const DEFAULT = "default";
+const OUTLINE = "outline";
+
+const Button = ({ type, text, onClick, disabled }) => {
+  return (
+    <button
+      type="button" 
+      className={cx("button", type)}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: DEFAULT,
+  disabled: false,
+  onClick: () => {},
+};
+Button.propType = {
+  type: PropTyps.oneOf([DEFAULT, OUTLINE]),
+  text: PropTyps.string.isRequired,
+  disabled: PropTyps.bool,
+  onClick: PropTyps.func,
+};
+
+export default Button;
