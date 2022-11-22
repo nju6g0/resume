5f201c56367d723587c0522ebe0015622d36ba08
YiShan Su <nju6g0@gmail.com>
2022-11-22 11:48:28 +0800

    (f2e_scrum_productBacklog) scene PRACTICAL

--- src/Pages/TheF2E/Scrum/ProductBacklog.js
+++ src/Pages/TheF2E/Scrum/ProductBacklog.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { nanoid } from "nanoid";
 
 import img_jira from "image/F2E_scrum/jira_logo.png";
 import Button, { BUTTON_TYPES } from "./Button";
 import Dialog from "./Dialog";
 import Role, { ROLE_NAMES } from "./Role";
-import List, { LIST_THEME, LIST_ITEM_TYPE } from "./List";
+import { Context, STEPS_KEY } from "./Scrum2";
+import List, { LIST_ITEM_TYPE } from "./List";
 
 import classes from "./styles.module.scss";
 import classNames from "classnames/bind";
@@ -14,6 +17,7 @@ const INTRO = "intro";
 const READY = "ready";
 const DEMO = "demo";
 const PRACTICAL = "practical";
+const FINISHED = "finished";
 const DIALOG_CONTENT = {
   [INTRO]: (
     <p>
@@ -44,11 +48,36 @@ const DIALOG_CONTENT = {
       <p>提示 ： 請把需求拖移至產品待辦清單 ， 並調整其優先順序 。</p>
     </>
   ),
+  [FINISHED]: <p>哇喔完成惹 ， 尼太棒ㄌ！ 我們繼續吧 ！</p>,
 };
 
 const ProductBacklog = () => {
+  const context = useContext(Context);
   const [nowScene, setNowScene] = useState(INTRO);
   const [isDemoActive, setIsDemoActive] = useState(false);
+  const [listItems, setListItems] = useState({
+    left: [
+      {
+        title: "應徵者的線上履歷編輯器",
+        id: nanoid()
+      },
+      {
+        title: "後台職缺管理功能（資訊上架、下架、顯示應徵者資料）",
+        id: nanoid(),
+      },
+    ],
+    center: [],
+    right: [
+      {
+        title: "會員系統（登入、註冊、權限管理）",
+        id: nanoid(),
+      },
+      {
+        title: "前台職缺列表、應徵",
+        id: nanoid()
+      },
+    ],
+  });
 
   const handleClickContinue = () => {
     if (nowScene === INTRO) {
@@ -58,20 +87,46 @@ const ProductBacklog = () => {
       if (!isDemoActive) {
         window.scrollTo({
           top: 500,
-          behavior: "smooth"
-        })
+          behavior: "smooth",
+        });
         setIsDemoActive(true);
       } else {
         window.scrollTo({
           top: 0,
-          behavior: "smooth"
-        })
+          behavior: "smooth",
+        });
         setNowScene(PRACTICAL);
       }
+    }
+    if (nowScene === FINISHED) {
+      context.goStep(STEPS_KEY.PLANNING);
     }
   };
   const handleClickReady = () => {
     setNowScene(DEMO);
+  };
+  const handleClickFinished = () => {
+    setNowScene(FINISHED);
+  };
+  const handleDragEnd = (event) => {
+    const { source, destination } = event;
+
+    if (!destination) {
+      return;
+    }
+
+    let draggedItems = { ...listItems };
+    const [dragTarget] = draggedItems[source.droppableId].splice(
+      source.index,
+      1
+    );
+    draggedItems[destination.droppableId].splice(
+      destination.index,
+      0,
+      dragTarget
+    );
+
+    setListItems(draggedItems);
   };
 
   const renderContent = () => {
@@ -84,7 +139,7 @@ const ProductBacklog = () => {
         return (
           <div className={cx("demo")}>
             <div>
-              <div className={cx("left", "disabled")}>
+              <div className={cx("disabled")}>
                 <List.Item>應徵者的線上履歷編輯器</List.Item>
               </div>
               <div className={cx("disabled")}>
@@ -103,7 +158,7 @@ const ProductBacklog = () => {
               <div className={cx("disabled")}>
                 <List.Item>會員系統（登入、註冊、權限管理）</List.Item>
               </div>
-              <div className={cx("right", isDemoActive && "dragTarget")}>
+              <div className={cx(isDemoActive && "dragTarget")}>
                 <List.Item>前台職缺列表、應徵</List.Item>
               </div>
             </div>
@@ -111,33 +166,105 @@ const ProductBacklog = () => {
         );
       case PRACTICAL:
         return (
-          <div className={cx("demo")}>
-            <div>
-              <div className={cx("left")}>
-                <List.Item>應徵者的線上履歷編輯器</List.Item>
-              </div>
-              <div>
-                <List.Item>
-                  後台職缺管理功能（資訊上架、下架、顯示應徵者資料）
-                </List.Item>
-              </div>
+          <>
+            <div className={cx("practical")}>
+              <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId="left">
+                  {(provided) => (
+                    <div
+                      className={cx("dropConatiner")}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      {listItems.left.map((item, idx) => (
+                        <div key={item.id}>
+                          <Draggable
+                            draggableId={item.id}
+                            index={idx}
+                            key={item.id}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                ref={provided.innerRef}
+                              >
+                                <List.Item>{item.title}</List.Item>
+                              </div>
+                            )}
+                          </Draggable>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </Droppable>
+                <List title="產品待辦清單" subtitle="Product Backlog">
+                  <Droppable droppableId="center">
+                    {(provided) => (
+                      <div
+                        className={cx("dropConatiner")}
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                      >
+                        {listItems.center.map((item, idx) => (
+                          <div key={item.id}>
+                            <Draggable
+                              draggableId={item.id}
+                              index={idx}
+                              key={item.id}
+                            >
+                              {(provided, snapshot) => (
+                                <div
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  ref={provided.innerRef}
+                                >
+                                  <List.Item>{item.title}</List.Item>
+                                </div>
+                              )}
+                            </Draggable>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </Droppable>
+                </List>
+                <Droppable droppableId="right">
+                  {(provided) => (
+                    <div
+                      className={cx("dropConatiner")}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      {listItems.right.map((item, idx) => (
+                        <div key={item.id}>
+                          <Draggable
+                            draggableId={item.id}
+                            index={idx}
+                            key={item.id}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                ref={provided.innerRef}
+                              >
+                                <List.Item>{item.title}</List.Item>
+                              </div>
+                            )}
+                          </Draggable>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </Droppable>
+              </DragDropContext>
             </div>
-            <List title="產品待辦清單" subtitle="Product Backlog">
-              <List.Item type={LIST_ITEM_TYPE.DASHED}></List.Item>
-              <List.Item type={LIST_ITEM_TYPE.DASHED}></List.Item>
-              <List.Item type={LIST_ITEM_TYPE.DASHED}></List.Item>
-              <List.Item type={LIST_ITEM_TYPE.DASHED}></List.Item>
-            </List>
-            <div>
-              <div>
-                <List.Item>會員系統（登入、註冊、權限管理）</List.Item>
-              </div>
-              <div className={cx("right")}>
-                <List.Item>前台職缺列表、應徵</List.Item>
-              </div>
-            </div>
-          </div>
+            <Button text="我完成了" disabled={listItems.center.length !== 4} onClick={handleClickFinished} />
+          </>
         );
+      case FINISHED:
+        return <Button type={BUTTON_TYPES.OUTLINE} text="點擊畫面任意處繼續" />;
       default:
         return null;
     }
@@ -150,7 +277,9 @@ const ProductBacklog = () => {
         <div>
           <Dialog
             speaker="PO"
-            isShowTriangle={nowScene === INTRO || nowScene === DEMO}
+            isShowTriangle={
+              nowScene === INTRO || nowScene === DEMO || nowScene === FINISHED
+            }
             children={DIALOG_CONTENT[nowScene]}
           />
         </div>
