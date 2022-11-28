c238002e4db76ed86109ec1e29fd419e24d48381
YiShan Su <nju6g0@gmail.com>
2022-11-29 00:30:32 +0800

    (footer) add designer

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -3,16 +3,30 @@ import qs from "qs";
 import { useLocation } from "react-router-dom";
 import { GiCherish } from "react-icons/gi";
 
+import { WEB as QUERY_TAB, SCRUM as QUERY_SCRUM } from 'Pages/TheF2E';
+
 import classes from "./styles.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(classes);
+
+const DESIGNER = {
+  [QUERY_TAB]: {
+    name: 'irena',
+    link: 'https://2022.thef2e.com/users/12061549261454740005'
+  },
+  [QUERY_SCRUM]: {
+    name: 'EG',
+    link: 'https://2022.thef2e.com/users/12061549261454740203'
+  }
+}
 
 const Footer = () => {
   const location = useLocation();
   const querys = qs.parse(location.search, { ignoreQueryPrefix: true });
   const path = location.pathname.replace(/^\//, "");
 
-  const hasDesigner = path === "thef2e" && querys.tab === "scrum";
+  const hasDesigner = path === "thef2e" && [QUERY_SCRUM, QUERY_TAB].some(tab => tab === querys.tab);
+  const queryTab = querys.tab;
 
   return (
     <div className={cx("footer")}>
@@ -21,11 +35,11 @@ const Footer = () => {
       {hasDesigner && (
         <>
           <a
-            href="https://2022.thef2e.com/users/12061549261454740203"
+            href={DESIGNER[queryTab].link}
             target="_blank"
             rel="noreferrer"
           >
-            UI Design - EG
+            UI Design - {DESIGNER[queryTab].name}
           </a>
           &nbsp;/
         </>
