ce5cd634c11827d358e57638b5d0dd10259f9f50
Nicole Su <nju6g0@gmail.com>
2023-12-05 16:34:54 +0800

    (snake)handle riched target

--- src/Pages/Games/Snake/index.js
+++ src/Pages/Games/Snake/index.js
@@ -5,53 +5,74 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(classes);
 
 const COLUM = 20;
-const RIGHT = 'ArrowRight';
-const LEFT = 'ArrowLeft';
-const UP = 'ArrowUp';
-const DOWN = 'ArrowDown';
+const RIGHT = "ArrowRight";
+const LEFT = "ArrowLeft";
+const UP = "ArrowUp";
+const DOWN = "ArrowDown";
 
 const Snake = () => {
-  const [snake, setSnake] = useState([{ x: 3, y: 5 },{ x: 3, y: 4 },{ x: 3, y: 3 }]);
-  const [fruit, setFruit] = useState({x: 9, y: 12});
+  const [snake, setSnake] = useState([
+    { x: 3, y: 5 },
+    { x: 3, y: 4 },
+    { x: 3, y: 3 },
+  ]);
+  const [fruit, setFruit] = useState({ x: 9, y: 12 });
   const [direction, setDirection] = useState(RIGHT);
 
   useEffect(() => {
     const timer = setInterval(() => {
-        let head = {...snake[0]};
-        if(head.x >= COLUM || head.x <= 1 || head.y >= COLUM || head.y <= 1) return;
-        if(direction === RIGHT){
-            head.y += 1
-        }
-        if(direction === LEFT){
-            head.y -= 1
-        }
-        if(direction === UP){
-            head.x -= 1
-        }
-        if(direction === DOWN){
-            head.x += 1
-        }
-        const snakeCopy = [head, ...snake];
+      let head = { ...snake[0] };
+      if (direction === RIGHT) {
+        head.y += 1;
+      }
+      if (direction === LEFT) {
+        head.y -= 1;
+      }
+      if (direction === UP) {
+        head.x -= 1;
+      }
+      if (direction === DOWN) {
+        head.x += 1;
+      }
+
+      if (head.x > COLUM || head.x < 1 || head.y > COLUM || head.y < 1)
+      return;
+      const snakeCopy = [head, ...snake];
+      if (head.x === fruit.x && head.y === fruit.y) {
+        const newFruit = {
+            x: Math.floor(Math.random() * 20) + 1,
+            y: Math.floor(Math.random() * 20) + 1,
+          }
+          console.log(newFruit)
+        setFruit(newFruit);
+      } else {
         snakeCopy.pop();
-        setSnake(snakeCopy)
+      }
+      setSnake(snakeCopy);
     }, 200);
 
     return () => {
-        clearInterval(timer);
-    }
-  }, [snake, direction])
+      clearInterval(timer);
+    };
+  }, [snake, direction, fruit]);
 
   useEffect(() => {
-    const handleChangeDirection = (e) => {
-        setDirection(e.key)
-    }
-    window.addEventListener('keydown', handleChangeDirection);
+    const handleChangeDirection = ({ key }) => {
+      if (
+        (key === LEFT && direction === RIGHT) ||
+        (key === RIGHT && direction === LEFT) ||
+        (key === UP && direction === DOWN) ||
+        (key === DOWN && direction === UP)
+      )
+        return;
+      setDirection(key);
+    };
+    window.addEventListener("keydown", handleChangeDirection);
 
     return () => {
-        window.removeEventListener('keydown', handleChangeDirection);
-    }
-  },[])
-
+      window.removeEventListener("keydown", handleChangeDirection);
+    };
+  }, [direction]);
 
   return (
     <div className={cx("container")}>
@@ -59,15 +80,14 @@ const Snake = () => {
         {Array.from({ length: COLUM }, (_, i) => i + 1).map((colX) => (
           <div key={`x_${colX}`} className={cx("row")}>
             {Array.from({ length: COLUM }, (_, i) => i + 1).map((colY) => {
-                let type;
-                if(snake.some(el => el.x === colX && el.y === colY)){
-                    type = 'snake';
-                } else if(colX === fruit.x && colY === fruit.y){
-                    type = 'fruit';
-                }
-                return(
-              <div key={`y_${colY}`} className={cx("colum", type)} />
-            )})}
+              let type;
+              if (snake.some((el) => el.x === colX && el.y === colY)) {
+                type = "snake";
+              } else if (colX === fruit.x && colY === fruit.y) {
+                type = "fruit";
+              }
+              return <div key={`y_${colY}`} className={cx("colum", type)} />;
+            })}
           </div>
         ))}
       </div>
