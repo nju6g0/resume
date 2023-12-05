4057e02edfa4848ff5f63b2ba3a35fccbfe4fdc3
Nicole Su <nju6g0@gmail.com>
2023-12-05 15:23:30 +0800

    (snake)basic setting

--- src/Pages/Games/Snake/index.js
+++ src/Pages/Games/Snake/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import classes from './styles.module.scss';
-import classNames from 'classnames/bind';
+import classes from "./styles.module.scss";
+import classNames from "classnames/bind";
 const cx = classNames.bind(classes);
 
+const getRandomKey = () => Math.floor(Math.random() * 10000);
+
 const Snake = () => {
-    return (
-        <div className={cx('wrap')}>
-            <h1>Snake</h1>
-        </div>
-    );
+  const [snake, setSnake] = useState([{ x: 3, y: 5 },{ x: 3, y: 4 },{ x: 3, y: 3 }]);
+  const [fruit, setFruit] = useState({x: 9, y: 12})
+  return (
+    <div className={cx("container")}>
+      <div>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((colX) => (
+          <div key={getRandomKey()} className={cx("row")}>
+            {Array.from({ length: 20 }, (_, i) => i + 1).map((colY) => {
+                let type;
+                if(snake.some(el => el.x === colX && el.y === colY)){
+                    type = 'snake';
+                } else if(colX === fruit.x && colY === fruit.y){
+                    type = 'fruit';
+                }
+                return(
+              <div key={getRandomKey()} className={cx("colum", type)} />
+            )})}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Snake;
